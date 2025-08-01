846d03b41009e12c2a3def4a332ab7280b896c60
alexisZC19 <alexander.von122@gmail.com>
2025-08-01 14:24:36 -0600

    Conexion a la bd con myssql2

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,24 +1,9 @@
+// src/lib/db.ts
 import mysql from 'mysql2/promise';
 
-declare global {
-  var connection: mysql.Pool | undefined;
-}
-
-if (!process.env.MYSQL_HOST) throw new Error("MYSQL_HOST is not defined");
-if (!process.env.MYSQL_USER) throw new Error("MYSQL_USER is not defined");
-if (!process.env.MYSQL_PASSWORD) throw new Error("MYSQL_PASSWORD is not defined");
-if (!process.env.MYSQL_DATABASE) throw new Error("MYSQL_DATABASE is not defined");
-
-
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+export const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'agendaicapet',
 });
-
-
-export const db = pool;
